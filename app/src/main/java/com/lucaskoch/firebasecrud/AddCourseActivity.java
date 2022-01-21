2441e2e46e6dda0b91c8fcd5d66ba87c8da9ced6
Lucas Koch <luko907@hotmail.com>
2022-01-21 13:21:39 -0300

    hola

--- app/src/main/java/com/lucaskoch/firebasecrud/AddCourseActivity.java
+++ app/src/main/java/com/lucaskoch/firebasecrud/AddCourseActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class AddCourseActivity extends AppCompatActivity {
+    TextView hola;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
