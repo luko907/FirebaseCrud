0868a438db572d8ecb2e672eeea57cf3fb3a22ca
Lucas Koch <luko907@hotmail.com>
2022-01-21 12:56:07 -0300

    Initial commit

---
+++ app/src/main/java/com/lucaskoch/firebasecrud/AddCourseActivity.java
@@ -0,0 +1,15 @@
+package com.lucaskoch.firebasecrud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AddCourseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_course);
+
+    }
+}
